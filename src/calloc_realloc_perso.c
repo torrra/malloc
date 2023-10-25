3bbe1501348da5ae3235b99074b00fbb9ea89443
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2023-10-25 15:37:16 -0400

    Fix realloc_perso and calloc_perso functions
    Dereference new_data pointer in realloc_perso to properly initialize it
    Cast block_data pointer to char* instead of int* in calloc_perso, and
    initialize each byte to 0 (size * nb_elements times)

--- src/calloc_realloc_perso.c
+++ src/calloc_realloc_perso.c
@@ -22,26 +22,20 @@ void* realloc_perso(void* to_realloc, size_t new_size)
         new_data_copy[index] = *((char*)to_realloc);
         to_realloc = ((char*) to_realloc) + 1;
     }
-
-    new_data = new_data_copy;
+    to_realloc = NULL;
+    *new_data = *new_data_copy;
     return new_data;
 }
 
 void* calloc_perso(size_t nb_elements, size_t size)
 {
-    int*    block_data = (int*) malloc_perso(size);
+    char*    block_data = (char*) malloc_perso(size);
 
     if (NULL == block_data)
-        return;
-
-    int     initialized_arr[size];
-    
-    for (int index = 0; index < size; index++)
+        return NULL;
+    for (size_t index = 0; index < size * nb_elements; index++)
     {
-        initialized_arr[index] = 0;
+        *(block_data + index) = 0;
     }
-
-    block_data = initialized_arr;
-
     return (void*) block_data;
 }
