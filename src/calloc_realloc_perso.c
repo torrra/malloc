8eac3dad80b377ddedea5705d9cc5ad790fd8d73
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2023-11-01 11:08:16 -0400

    Add comments to calloc_realloc_perso.c src file
    Add comments in both realloc_perso and calloc function definitions

--- src/calloc_realloc_perso.c
+++ src/calloc_realloc_perso.c
@@ -6,37 +6,50 @@
 
 void* realloc_perso(void* to_realloc, size_t new_size)
 {
+    // Free block to either split it if large enough or leave it free if not
     free_perso(to_realloc);
 
+    // Cast new data pointer to char* for later pointer arithmetics (since char is 1 byte)
     char*   new_data = (char*) malloc_perso(new_size);
 
+    // Return NULL if allocation failed
     if (NULL == new_data)
         return NULL;
 
+    // Use array of block size to get each byte of data
     char    new_data_copy[((t_block*) to_realloc - 1)->m_size];
 
-    to_realloc = (char*) to_realloc;
-
-    for (size_t index = 0; index < ((t_block*) ((__intptr_t) to_realloc + (sizeof(t_block))))->m_size && 
+    // For each byte, copy it into the array and increment pointer to copy next byte
+    for (size_t index = 0; index < ((t_block*) ((char*) to_realloc + (sizeof(t_block))))->m_size && 
     index < new_size; index++)
     {
         new_data_copy[index] = *((char*)to_realloc);
         to_realloc = ((char*) to_realloc) + 1;
     }
+
+    // Set argument to NULL
     to_realloc = NULL;
+
+    // Copy data from array into new data pointer
     *new_data = *new_data_copy;
+
+    // Return new data pointer
     return new_data;
 }
 
 void* calloc_perso(size_t nb_elements, size_t size)
 {
+    // Cast new data pointer to char* for pointer arithmetics
     char*    block_data = (char*) malloc_perso(nb_elements * size);
 
+    // Return NULL if allocation failed
     if (NULL == block_data)
         return NULL;
+    
+    // Initialize nb_elements * size bytes to 0
     for (size_t index = 0; index < size * nb_elements; index++)
-    {
         *(block_data + index) = 0;
-    }
+
+    // Return data array as pointer to void
     return (void*) block_data;
 }
